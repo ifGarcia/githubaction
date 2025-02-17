8fb3766c5c0a163da04e04dc77621fec8b49743c
Bruno Vinicius Garcia <44682187+ifGarcia@users.noreply.github.com>
2025-02-17 15:49:00 -0300

    Create generate_badge.js

---
+++ .github/script/generate_badge.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const badge = `
+<svg xmlns="http://www.w3.org/2000/svg" width="100" height="20">
+  <rect width="100" height="20" fill="#555"/>
+  <rect x="37" width="63" height="20" fill="#4c1"/>
+  <text x="19.5" y="14" fill="#fff" font-family="Verdana" font-size="11">build</text>
+  <text x="50" y="14" fill="#fff" font-family="Verdana" font-size="11">passing</text>
+</svg>`;
+fs.writeFileSync('badge.svg', badge);
