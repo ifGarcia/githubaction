a0e41607eef44507fc9e6d3ae295cfcd0bc4d3ac
Bruno Vinicius Garcia <44682187+ifGarcia@users.noreply.github.com>
2025-02-19 09:09:56 -0300

    Update generate_badge.js

--- .github/script/generate_badge.js
+++ .github/script/generate_badge.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
+
+const envName = process.env.environment;
+const version = process.env.version;
+const repo = process.env.repo;
+
 const badge = `
 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="20">
   <rect width="100" height="20" fill="#555"/>
   <rect x="37" width="63" height="20" fill="#4c1"/>
-  <text x="19.5" y="14" fill="#fff" font-family="Verdana" font-size="11">${{ env.environment }}</text>
-  <text x="50" y="14" fill="#fff" font-family="Verdana" font-size="11">${{ env.version }}</text>
+  <text x="19.5" y="14" fill="#fff" font-family="Verdana" font-size="11">${environment}</text>
+  <text x="50" y="14" fill="#fff" font-family="Verdana" font-size="11">${version}</text>
 </svg>`;
-fs.writeFileSync('badge.svg', badge);
+
+
+const fileName = `${repo.toLowerCase().replace(/\s+/g, '_')}/${envName.toLowerCase().replace(/\s+/g, '_')}.svg`;
+fs.writeFileSync(fileName, badge);
